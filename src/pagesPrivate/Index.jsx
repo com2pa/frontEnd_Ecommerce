07694a97b63bb6f451871d0b3d8db9e1194909e8
merwil vegas <com2pa@gmail.com>
2025-05-12 19:53:09 -0400

    pagina de inicio dashboard

---
+++ src/pagesPrivate/Index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import SidebarHeader from './LayoutPrivate/SidebarHeader';
+const Index = () => {
+  return (
+    <SidebarHeader>
+      <></>
+    </SidebarHeader>
+  );
+}
+
+export default Index
