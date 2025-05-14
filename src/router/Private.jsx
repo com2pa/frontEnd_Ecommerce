046df09b043ec70067539ef2fe1b49ec21de18e7
merwil vegas <com2pa@gmail.com>
2025-05-14 05:50:57 -0400

    se añadio la ruta de la alicuota

--- src/router/Private.jsx
+++ src/router/Private.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
-
+import { Route, Routes } from 'react-router'
+import PersistAuth from '../components/PersistAuth';
+import Index from '../pagesPrivate/Index';
+import Category from '../pages/category';
+import Subcategory from '../pages/Subcategory';
+import Aliquots from '../pagesPrivate/Aliquots';
 const Private = () => {
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <Routes>
+        <Route element={<PersistAuth />}>
+          <Route path='/dashboard' element={<Index />}/>
+          <Route path='/category' element={<Category />} />
+          <Route path='/subcategory' element={<Subcategory />} />
+          <Route path='/aliquots' element={<Aliquots/>} />
+        </Route>
+      </Routes>
+    </>
+  );
 }
 
 export default Private
