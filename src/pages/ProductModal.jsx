5580c09c050cea69ea1b1e0245c7285a19f07812
merwil vegas <com2pa@gmail.com>
2025-05-14 05:46:09 -0400

    vista de modal para editar y eliminar

---
+++ src/pages/ProductModal.jsx
@@ -0,0 +1,248 @@
+import React from 'react';
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  ModalFooter,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Textarea,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  NumberIncrementStepper,
+  NumberDecrementStepper,
+  Select,
+  Switch,
+  Flex,
+//   useToast,
+} from '@chakra-ui/react';
+
+const ProductModal = ({
+  isOpen,
+  onClose,
+  product,
+  onUpdate,
+  brands,
+  subcategories,
+  aliquots,
+}) => {
+  const [editingProduct, setEditingProduct] = React.useState(product);
+  const [isLoading, setIsLoading] = React.useState(false);
+//   const toast = useToast();
+
+  React.useEffect(() => {
+    setEditingProduct(product);
+  }, [product]);
+
+  const handleInputChange = (e) => {
+    const { name, value, type, checked } = e.target;
+    setEditingProduct((prev) => ({
+      ...prev,
+      [name]: type === 'checkbox' ? checked : value,
+    }));
+  };
+
+  const handleNumberChange = (name, value) => {
+    setEditingProduct((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async () => {
+    setIsLoading(true);
+    try {
+      const success = await onUpdate(editingProduct);
+      if (success) {
+        onClose();
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      onClose={onClose}
+      size='xl'
+    >
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Editar Producto</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <Flex
+            direction='column'
+            gap={4}
+          >
+            <FormControl>
+              <FormLabel>Nombre</FormLabel>
+              <Input
+                name='name'
+                value={editingProduct?.name || ''}
+                onChange={handleInputChange}
+                placeholder='Nombre del producto'
+              />
+            </FormControl>
+
+            <FormControl>
+              <FormLabel>Descripción</FormLabel>
+              <Textarea
+                name='description'
+                value={editingProduct?.description || ''}
+                onChange={handleInputChange}
+                placeholder='Descripción del producto'
+              />
+            </FormControl>
+
+            <Flex gap={4}>
+              <FormControl>
+                <FormLabel>Precio</FormLabel>
+                <NumberInput
+                  value={editingProduct?.price || 0}
+                  onChange={(value) => handleNumberChange('price', value)}
+                  min={0}
+                  precision={2}
+                >
+                  <NumberInputField />
+                  <NumberInputStepper>
+                    <NumberIncrementStepper />
+                    <NumberDecrementStepper />
+                  </NumberInputStepper>
+                </NumberInput>
+              </FormControl>
+
+              <FormControl>
+                <FormLabel>Stock</FormLabel>
+                <NumberInput
+                  value={editingProduct?.stock || 0}
+                  onChange={(value) => handleNumberChange('stock', value)}
+                  min={0}
+                >
+                  <NumberInputField />
+                  <NumberInputStepper>
+                    <NumberIncrementStepper />
+                    <NumberDecrementStepper />
+                  </NumberInputStepper>
+                </NumberInput>
+              </FormControl>
+
+              <FormControl>
+                <FormLabel>Stock Mínimo</FormLabel>
+                <NumberInput
+                  value={editingProduct?.minStock || 0}
+                  onChange={(value) => handleNumberChange('minStock', value)}
+                  min={0}
+                >
+                  <NumberInputField />
+                  <NumberInputStepper>
+                    <NumberIncrementStepper />
+                    <NumberDecrementStepper />
+                  </NumberInputStepper>
+                </NumberInput>
+              </FormControl>
+            </Flex>
+
+            <Flex gap={4}>
+              <FormControl>
+                <FormLabel>Marca</FormLabel>
+                <Select
+                  name='brandId'
+                  value={editingProduct?.brandId || ''}
+                  onChange={handleInputChange}
+                >
+                  <option value=''>Seleccione una marca</option>
+                  {brands.map((brand) => (
+                    <option
+                      key={brand.id}
+                      value={brand.id}
+                    >
+                      {brand.name}
+                    </option>
+                  ))}
+                </Select>
+              </FormControl>
+
+              <FormControl>
+                <FormLabel>Subcategoría</FormLabel>
+                <Select
+                  name='subcategoryId'
+                  value={editingProduct?.subcategoryId || ''}
+                  onChange={handleInputChange}
+                >
+                  <option value=''>Seleccione una subcategoría</option>
+                  {subcategories.map((sub) => (
+                    <option
+                      key={sub.id}
+                      value={sub.id}
+                    >
+                      {sub.name}
+                    </option>
+                  ))}
+                </Select>
+              </FormControl>
+            </Flex>
+
+            <Flex gap={4}>
+              <FormControl>
+                <FormLabel>Aliquot</FormLabel>
+                <Select
+                  name='aliquotId'
+                  value={editingProduct?.aliquotId || ''}
+                  onChange={handleInputChange}
+                >
+                  <option value=''>Seleccione un aliquot</option>
+                  {aliquots.map((aliquot) => (
+                    <option
+                      key={aliquot.id}
+                      value={aliquot.id}
+                    >
+                      {aliquot.name} {aliquot.code} - {aliquot.percentage}%
+                    </option>
+                  ))}
+                </Select>
+              </FormControl>
+
+              <FormControl>
+                <FormLabel>Estado</FormLabel>
+                <Switch
+                  name='isActive'
+                  isChecked={editingProduct?.isActive || false}
+                  onChange={handleInputChange}
+                  colorScheme='blue'
+                >
+                  {editingProduct?.isActive ? 'Activo' : 'Inactivo'}
+                </Switch>
+              </FormControl>
+            </Flex>
+          </Flex>
+        </ModalBody>
+        <ModalFooter>
+          <Button
+            colorScheme='blue'
+            mr={3}
+            onClick={handleSubmit}
+            isLoading={isLoading}
+          >
+            Guardar Cambios
+          </Button>
+          <Button
+            variant='ghost'
+            onClick={onClose}
+          >
+            Cancelar
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+};
+
+export default ProductModal;
