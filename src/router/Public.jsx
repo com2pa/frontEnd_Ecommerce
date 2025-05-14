ab4fe1d7ec434910dc11dec3753f30fc8a5f1fd9
merwil vegas <com2pa@gmail.com>
2025-05-14 05:49:59 -0400

    se añadio endpoint marca y producto

--- src/router/Public.jsx
+++ src/router/Public.jsx
@@ -4,26 +4,30 @@ import {  Route, Routes } from 'react-router-dom';
 import Verify from '../pages/verify';
 import PersistAuth from '../components/PersistAuth';
 // import Registro from "../pages/Login/Registro";
-// import Login from "../pages/Login/Login";
 import Home from '../pages/Home';
+import Login from '../pages/Login';
+import Register from '../pages/Register';
+import Brand from '../pages/Brand';
+import Product from '../pages/Product';
 // import { QuienesSomos } from '../pages/QuienesSomos';
 
 export const Public = () => {
   return (
     <>
       <Routes>
-        <Route element={<PersistAuth/>}>
-          <Route path='/' element={<Home/>} />   
-        </Route> 
-        
-        {/* <Route path='/login' element={<Login/>} />
+        <Route element={<PersistAuth />}>
+          <Route path='/' element={<Home />}/>
+        </Route>
 
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/brand' element={<Brand />} />
+        <Route path='/product' element={<Product/> } />
+        {/* 
         <Route path='/verify/:id/:token' element={<Verify />} />
         <Route path='/registro' element={<Registro/>}/>
         <Route path='/somos' element={<QuienesSomos/>} /> */}
-              
       </Routes>
-
     </>
   );
 
