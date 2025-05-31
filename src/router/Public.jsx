9adc53abdc3f9bc8d7ae6ebf3e9dcab7c18fea08
merwil vegas <com2pa@gmail.com>
2025-05-31 07:33:08 -0400

    se elimino la ruta de producto

--- src/router/Public.jsx
+++ src/router/Public.jsx
@@ -8,8 +8,7 @@ import Home from '../pages/Home';
 import Login from '../pages/Login';
 import Register from '../pages/Register';
 import Brand from '../pages/Brand';
-import Product from '../pages/Product';
-// import { QuienesSomos } from '../pages/QuienesSomos';
+
 
 export const Public = () => {
   return (
@@ -22,10 +21,8 @@ export const Public = () => {
         <Route path='/login' element={<Login />} />
         <Route path='/register' element={<Register />} />
         <Route path='/brand' element={<Brand />} />
-        <Route path='/product' element={<Product/> } />
-        {/* 
         <Route path='/verify/:id/:token' element={<Verify />} />
-        <Route path='/registro' element={<Registro/>}/>
+         {/* <Route path='/registro' element={<Registro/>}/>
         <Route path='/somos' element={<QuienesSomos/>} /> */}
       </Routes>
     </>
