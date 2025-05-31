96979d99e2d8bf3076f9bab04fb8e7ce89ee963d
merwil vegas <com2pa@gmail.com>
2025-05-31 07:32:40 -0400

    se añadio ruta de producto

--- src/router/Private.jsx
+++ src/router/Private.jsx
@@ -5,6 +5,7 @@ import Index from '../pagesPrivate/Index';
 import Category from '../pages/category';
 import Subcategory from '../pages/Subcategory';
 import Aliquots from '../pagesPrivate/Aliquots';
+import Product from '../pagesPrivate/Product';
 const Private = () => {
   return (
     <>
@@ -14,6 +15,7 @@ const Private = () => {
           <Route path='/category' element={<Category />} />
           <Route path='/subcategory' element={<Subcategory />} />
           <Route path='/aliquots' element={<Aliquots/>} />
+          <Route path='/product' element={<Product/> } />
         </Route>
       </Routes>
     </>
