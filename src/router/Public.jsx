2f974c9923ecf4d9168e685afb803989dc5c3212
merwil vegas <com2pa@gmail.com>
2025-06-01 16:23:36 -0400

    rutas

--- src/router/Public.jsx
+++ src/router/Public.jsx
@@ -11,15 +11,17 @@ import Categoria from '../pages/Home/Categoria';
 import Subcategorias from '../pages/Home/SubCategoria';
 import Productos from '../pages/Home/Productos';
 import DetalleProducto from '../pages/Home/DetalleProducto';
+import Index from '../pagesPrivate/Index';
 
 
 export const Public = () => {
   return (
     <>
       <Routes>
-        {/* <Route element={<PersistAuth />}> */}
+        <Route element={<PersistAuth />}>
+          <Route path='/' element={<Index />} />
           <Route path='/home' element={<Home />}/>
-        {/* </Route> */}
+        </Route>
         <Route path='/login' element={<Login />} />
         <Route path='/register' element={<Register />} />       
         <Route path='/verify/:id/:token' element={<Verify />} />
