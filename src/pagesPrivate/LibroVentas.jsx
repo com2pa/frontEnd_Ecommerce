afca3a0d22bc9aa17e4087466c282471777c80f6
merwil vegas <com2pa@gmail.com>
2025-06-14 12:52:33 -0400

    en desarrollo vista de libro de ventas.

---
+++ src/pagesPrivate/LibroVentas.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SidebarHeader from './LayoutPrivate/SidebarHeader'
+
+const LibroVentas = () => {
+  return (
+    <SidebarHeader>
+        
+    </SidebarHeader>
+  )
+}
+
+export default LibroVentas
