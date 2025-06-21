ab4dbfd45901693faee353cdcacec604d9ff180d
merwil vegas <com2pa@gmail.com>
2025-06-21 17:49:20 -0400

    llamando al componente de desciento

---
+++ src/pages/Ofertas.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Descuento from './Home/Descuento'
+import Menu from '../layout/Menu'
+import PiePagina from '../layout/PiePagina'
+const Ofertas = () => {
+  return (
+    <>
+    <Menu/>
+        <Descuento/>
+    <PiePagina/>
+    </>
+  )
+}
+
+export default Ofertas
