f8cb976425d02ca3f1c24294e1b4f282772445fe
merwil vegas <com2pa@gmail.com>
2025-07-05 15:53:09 -0400

    se cambio la imagen del usuario en menu

--- src/pagesPrivate/LayoutPrivate/DashboardCliente.jsx
+++ src/pagesPrivate/LayoutPrivate/DashboardCliente.jsx
@@ -229,11 +229,12 @@ const MobileNav = ({ onOpen, ...rest }) => {
             >
               <HStack>
                 <Avatar
-                  size={'sm'}
-                  src={
-                    auth?.avatar ||
-                    'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200&q=80'
-                  }
+                  size={'md'}
+                  // src={
+                  //   auth?.avatar ||
+                  //   'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200&q=80'
+                  // }
+                  name={auth.name}
                 />
                 <VStack
                   display={{ base: 'none', md: 'flex' }}
